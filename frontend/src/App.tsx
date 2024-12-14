f1e3639885725f274577e9672fe723caa9409155
Kabol Amine <amine.kabol@gmail.com>
2024-12-15 00:00:13 +0100

    mise en place de react via vite avec typescript + taillwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-lg shadow-lg">
+      <h1 className="">Hello, Tailwind CSS with Vite!</h1>
+      <p className="mt-2">Start building your app with Tailwind.</p>
+    </div>
+  );
+}
+
+export default App;
