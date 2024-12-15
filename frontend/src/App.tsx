69b621b02eb8bf37cd2e79b23c3c06bf3a027120
Kabol Amine <amine.kabol@gmail.com>
2024-12-15 14:06:23 +0100

    esquisse de la ProfilPage avant de connecter le back et le Front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 // import TasksPage from './pages/TasksPage';
-// import ProfilePage from './pages/ProfilePage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
 
@@ -14,9 +14,8 @@ function App() {
       <div className="min-h-screen pt-20 bg-gray-100 text-gray-800">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* {/* <Route path="/projects" element={<ProjectsPage />} /> */}
           <Route path="/projects" element={<ProjectsPage />} />
-          {/* <Route path="/profile" element={<ProfilePage />} /> */}
+          <Route path="/profile" element={<ProfilePage />} />
           <Route path="*" element={<h1 className="text-center text-3xl font-bold mt-20">404 - Page non trouvée</h1>} />
         </Routes>
       </div>
